b6404b7e5701c7e8fd2b906094c1e692f399c0f6
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-03 21:12:18 -0500

    second commit

---
+++ src/a/DemoA.java
@@ -0,0 +1,14 @@
+package a;
+
+public class DemoA {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is class A");
+		
+		System.out.println("update");
+		System.out.println("update successful");
+		
+	}
+
+}
