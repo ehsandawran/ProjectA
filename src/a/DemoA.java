f1177d443a49055843e8ee98dd3bafb5a9c44af4
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-03 21:30:29 -0500

    implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -6,8 +6,15 @@ public class DemoA {
 		
 		System.out.println("This is class A");
 		
-		System.out.println("update");
-		System.out.println("update successful");
+		System.out.println("Sprint 1 work ..");
+		System.out.println("print 2 work ..");
+
+		
+	}
+	
+	// Sprint 2 work
+	public static void add(int a, int b) {
+		System.out.println(a+b);
 		
 	}
 
