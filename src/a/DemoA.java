934b2b1804fbeeaa94a4a056e5e15fd3b94cf6d7
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-03 21:43:58 -0500

    added missing braces

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -1,30 +1,31 @@
 package a;
 
 public class DemoA {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("This is class A");
-		
+
 		System.out.println("Sprint 1 work ..");
 		System.out.println("print 2 work ..");
 
-		
 	}
-	
+
 	// Sprint 2 work
 	public static void add(int a, int b) {
-		System.out.println(a+b);
-		
+		System.out.println(a + b);
+
 	}
 
 	// Coming from GitHub
 	public static void subtract(int a, int b) {
-		System.out.println(a-b);
-
+		System.out.println(a - b);
+	}
 
 	// This is also GitHub
 	public static void mutiply(int a, int b) {
-		System.out.println(a*b);
+		System.out.println(a * b);
+
+	}
 
 }
